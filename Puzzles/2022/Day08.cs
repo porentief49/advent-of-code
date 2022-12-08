c4fb35410f25d581ff261c820f67528f20c7ed96
grr <rainer.gruber@mindscale.com>
2022-12-08 20:40:18 +0100

    all 2d LINQ now

--- Puzzles/2022/Day08.cs
+++ Puzzles/2022/Day08.cs
@@ -24,19 +24,18 @@ namespace Puzzles
                 int _xSize = InputData[0].Length;
                 int _ySize = InputData.Length;
                 int[][] _heights = new int[_ySize][];
+                bool[][] _visibleTrees = new bool[_ySize][];
                 int[][] _scenicScores = new int[_ySize][];
-                bool[][] _visible = new bool[_ySize][];
                 for (int i = 0; i < _ySize; i++)
                 {
                     _heights[i] = new int[_xSize];
+                    _visibleTrees[i] = new bool[_xSize];
                     _scenicScores[i] = new int[_xSize];
-                    _visible[i] = new bool[_xSize];
                 }
 
                 //parse all
-                for (int y = 0; y < _ySize; y++) _heights[y] = InputData[y].Select(l => int.Parse(l.ToString())).ToArray();
-
-                if (Part1) PrintIntGrid(_heights, 1);
+                _heights = InputData.Select(y => y.Select(l => int.Parse(l.ToString())).ToArray()).ToArray();
+                if (Part1 && !Verbose) PrintIntGrid(_heights, 1);
 
                 //check visibility / free area
                 for (int y = 0; y < _ySize; y++)
@@ -54,7 +53,7 @@ namespace Puzzles
                             while (x + _right < _xSize - 1 && _heights[y][x] > _heights[y][x + _right + 1]) _right++;
                             while (y - _up > 0 && _heights[y][x] > _heights[y - _up - 1][x]) _up++;
                             while (y + _down < _ySize - 1 && _heights[y][x] > _heights[y + _down + 1][x]) _down++;
-                            _visible[y][x] = _left == x || _right == _xSize - x - 1 || _up == y || _down == _ySize - y - 1;
+                            _visibleTrees[y][x] = _left == x || _right == _xSize - x - 1 || _up == y || _down == _ySize - y - 1;
                         }
                         else
                         {
@@ -67,35 +66,28 @@ namespace Puzzles
                     }
                 }
 
-                if (Part1) PrintBoolGrid(_visible);
-                else PrintIntGrid(_scenicScores, 3);
-
-                //count visibility
-                int _visibleTrees = 0;
-                int _bestScenicScore = 0;
-                for (int y = 0; y < _ySize; y++)
+                //find solution
+                if (Part1)
                 {
-                    if (Part1) _visibleTrees += _visible[y].Count(x => x == true);
-                    else _bestScenicScore = Math.Max(_bestScenicScore, _scenicScores[y].Max());
+                    if (!Verbose) PrintBoolGrid(_visibleTrees);
+                    return FormatResult(_visibleTrees.Select(y => y.Count(x => x == true)).Sum(), "visible trees");
                 }
-                if (Part1) return FormatResult(_visibleTrees, "visible trees");
-                return FormatResult(_bestScenicScore, "best scenic score");
+                if (!Verbose) PrintIntGrid(_scenicScores, 3);
+                return FormatResult(_scenicScores.Select(y => y.Max()).Max(), "best scenic score");
             }
 
             void PrintBoolGrid(bool[][] Grid)
             {
-                for (int y = 0; y < Grid.Length; y++) Console.WriteLine(Grid[y].Select(x => x ? 'x' : '.').ToArray());
+                //for (int y = 0; y < Grid.Length; y++) Console.WriteLine(Grid[y].Select(x => x ? 'x' : '.').ToArray());
+                Grid.ToList().ForEach(y => Console.WriteLine(y.Select(x => x ? 'x' : '.').ToArray()));
+                //for (int y = 0; y < Grid.Length; y++) Console.WriteLine(Grid[y].Select(x => x ? 'x' : '.').ToArray());
                 Console.WriteLine("");
             }
+
             void PrintIntGrid(int[][] Grid, int digits)
             {
-                for (int y = 0; y < Grid.Length; y++)
-                {
-                    //StringBuilder _sb = new();
-                    //for (int x = 0; x < Grid[y].GetLength(0); x++) _sb.Append(Grid[y][x].ToString() + " ".Repeat(digits).Substring(0, digits));
-                    //Console.WriteLine(_sb.ToString());
-                    Console.WriteLine(string.Join(' ', Grid[y].Select(x => x.ToString() + " ".Repeat(digits).Substring(0, digits)).ToArray()));
-                }
+                //for (int y = 0; y < Grid.Length; y++) Console.WriteLine(string.Join(' ', Grid[y].Select(x => x.ToString() + " ".Repeat(digits).Substring(0, digits)).ToArray()));
+                Grid.ToList().ForEach(y => Console.WriteLine(string.Join(' ', y.Select(x => x.ToString() + " ".Repeat(digits).Substring(0, digits)).ToArray())));
                 Console.WriteLine("");
             }
         }
