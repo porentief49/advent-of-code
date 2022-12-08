32774b19e6d87a1b5b6a6914d5597fbd8f99d268
grr <rainer.gruber@mindscale.com>
2022-12-08 19:56:37 +0100

    changed to jagged arrays

--- Puzzles/2022/Day08.cs
+++ Puzzles/2022/Day08.cs
@@ -23,18 +23,27 @@ namespace Puzzles
             {
                 int _xSize = InputData[0].Length;
                 int _ySize = InputData.Length;
-                int[,] _heights = new int[_xSize, _ySize];
-                int[,] _scenicScores = new int[_xSize, _ySize];
-                bool[,] _visible = new bool[_xSize, _ySize];
+                //int[][] _heights = new int[_xSize][_ySize];
+                //int[][] _scenicScores = new int[_xSize][_ySize];
+                //bool[][] _visible = new bool[_xSize][_ySize];
+                int[][] _heights = new int[_xSize][];
+                int[][] _scenicScores = new int[_xSize][];
+                bool[][] _visible = new bool[_xSize][];
+                for (int i = 0; i < _xSize; i++)
+                {
+                    _heights[i] = new int[_ySize];
+                    _scenicScores[i] = new int[_ySize];
+                    _visible[i] = new bool[_ySize];
+                }
 
                 //parse all
                 for (int y = 0; y < _ySize; y++)
                 {
                     for (int x = 0; x < _xSize; x++)
                     {
-                        _heights[x, y] = int.Parse(InputData[y][x].ToString());
-                        _visible[x, y] = false;
-                        _scenicScores[x, y] = 0;
+                        _heights[x][y] = int.Parse(InputData[y][x].ToString());
+                        _visible[x][y] = false;
+                        _scenicScores[x][y] = 0;
                     }
                 }
 
@@ -50,20 +59,20 @@ namespace Puzzles
 
                         if (Part1)
                         {
-                            while (x - _left > 0 && _heights[x, y] > _heights[x - _left - 1, y]) _left++;
-                            while (x + _right < _xSize - 1 && _heights[x, y] > _heights[x + _right + 1, y]) _right++;
-                            while (y - _up > 0 && _heights[x, y] > _heights[x, y - _up - 1]) _up++;
-                            while (y + _down < _ySize - 1 && _heights[x, y] > _heights[x, y + _down + 1]) _down++;
-                            _visible[x, y] = _left == x || _right == _xSize - x - 1 || _up == y || _down == _ySize - y - 1;
+                            while (x - _left > 0 && _heights[x][y] > _heights[x - _left - 1][y]) _left++;
+                            while (x + _right < _xSize - 1 && _heights[x][y] > _heights[x + _right + 1][y]) _right++;
+                            while (y - _up > 0 && _heights[x][y] > _heights[x][y - _up - 1]) _up++;
+                            while (y + _down < _ySize - 1 && _heights[x][y] > _heights[x][y + _down + 1]) _down++;
+                            _visible[x][y] = _left == x || _right == _xSize - x - 1 || _up == y || _down == _ySize - y - 1;
 
                         }
                         else
                         {
-                            if (x > 0) do _left++; while (x - _left > 0 && _heights[x, y] > _heights[x - _left, y]);
-                            if (x < _xSize - 1) do _right++; while (x + _right < _xSize - 1 && _heights[x, y] > _heights[x + _right, y]);
-                            if (y > 0) do _up++; while (y - _up > 0 && _heights[x, y] > _heights[x, y - _up]);
-                            if (y < _ySize - 1) do _down++; while (y + _down < _ySize - 1 && _heights[x, y] > _heights[x, y + _down]);
-                            _scenicScores[x, y] = _left * _right * _up * _down;
+                            if (x > 0) do _left++; while (x - _left > 0 && _heights[x][y] > _heights[x - _left][y]);
+                            if (x < _xSize - 1) do _right++; while (x + _right < _xSize - 1 && _heights[x][y] > _heights[x + _right][y]);
+                            if (y > 0) do _up++; while (y - _up > 0 && _heights[x][y] > _heights[x][y - _up]);
+                            if (y < _ySize - 1) do _down++; while (y + _down < _ySize - 1 && _heights[x][y] > _heights[x][y + _down]);
+                            _scenicScores[x][y] = _left * _right * _up * _down;
                         }
                     }
                 }
@@ -77,11 +86,11 @@ namespace Puzzles
                     {
                         if (Part1)
                         {
-                            if (_visible[x, y]) _visibleX++;
+                            if (_visible[x][y]) _visibleX++;
                         }
                         else
                         {
-                            if (_scenicScores[x, y] > _highest) _highest = _scenicScores[x, y];
+                            if (_scenicScores[x][y] > _highest) _highest = _scenicScores[x][y];
                         }
                     }
                 }
@@ -89,22 +98,22 @@ namespace Puzzles
                 return FormatResult(_highest, "best scenic score");
             }
 
-            void PrintBoolGrid(bool[,] Grid)
+            void PrintBoolGrid(bool[][] Grid)
             {
                 for (int y = 0; y < Grid.GetLength(1); y++)
                 {
                     StringBuilder _sb = new();
-                    for (int x = 0; x < Grid.GetLength(0); x++) _sb.Append(Grid[x, y] ? "x" : ".");
+                    for (int x = 0; x < Grid.GetLength(0); x++) _sb.Append(Grid[x][y] ? "x" : ".");
                     Console.WriteLine(_sb.ToString());
                 }
                 Console.WriteLine("");
             }
-            void PrintIntGrid(int[,] Grid, int digits)
+            void PrintIntGrid(int[][] Grid, int digits)
             {
                 for (int y = 0; y < Grid.GetLength(1); y++)
                 {
                     StringBuilder _sb = new();
-                    for (int x = 0; x < Grid.GetLength(0); x++) _sb.Append(Grid[x, y].ToString() + " ".Repeat(digits).Substring(0, digits));
+                    for (int x = 0; x < Grid.GetLength(0); x++) _sb.Append(Grid[x][y].ToString() + " ".Repeat(digits).Substring(0, digits));
                     Console.WriteLine(_sb.ToString());
                 }
                 Console.WriteLine("");
