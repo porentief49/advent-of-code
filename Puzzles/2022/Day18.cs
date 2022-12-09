70211483f612a8a04380efba3f4c46ba8abeb530
grr <rainer.gruber@mindscale.com>
2022-12-09 21:39:34 +0100

    add template classes for the remaining days

---
+++ Puzzles/2022/Day18.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Puzzles
+{
+    public partial class Year2022
+    {
+        public class Day18 : DayBase
+        {
+            protected override string Title { get; } = "Day 18:";
+
+            public override void SetupAll()
+            {
+                AddInputFile(@"2022\18_Example.txt");
+                //AddInputFile(@"2022\18_rAiner.txt");
+                //AddInputFile(@"2022\18_SEGCC.txt");
+            }
+
+            public override void Init(string InputFile) => InputData = ReadFile(InputFile, true);
+
+            public override string Solve(bool Part1)
+            {
+                return FormatResult(0, "not yet implemented");
+            }
+        }
+    }
+}
