2e96ab85e3d23619e793cb7faaaecab5f9b23c16
grr <rainer.gruber@mindscale.com>
2022-12-17 09:39:32 +0100

    17/1 working

--- Puzzles/2022/Day17.cs
+++ Puzzles/2022/Day17.cs
@@ -9,7 +9,7 @@ namespace Puzzles
     {
         public class Day17 : DayBase
         {
-            private const int cycles = 1;// 2022;
+            private const int cycles = 2022;
             private int currentTop;
             private List<Rock> rocks;
 
@@ -18,7 +18,7 @@ namespace Puzzles
             public override void SetupAll()
             {
                 AddInputFile(@"2022\17_Example.txt");
-                //AddInputFile(@"2022\17_rAiner.txt");
+                AddInputFile(@"2022\17_rAiner.txt");
                 //AddInputFile(@"2022\17_SEGCC.txt");
             }
 
@@ -32,61 +32,123 @@ namespace Puzzles
                 //List<string> chamber = new("+-------+");
                 rocks = new();
                 currentTop = 0; //floor
+                int streamIndex = 0;
                 for (int i = 1; i <= cycles; i++)
                 {
                     Rock rock = new(i);
                     rock.Y = currentTop + 3 + 1;
                     rock.X = 2;
+
                     rocks.Add(rock);
-                    PrintChamber();
-                    bool keepGoing = true;
-                    do
+                    //PrintChamber();
+
+                    //bool keepGoing = true;
+                    //do
+                    //{
+                    //    keepGoing = RockFalls();
+                    //} while (keepGoing);
+                    bool push = true;
+                    while (rock.Floating)
                     {
-                        keepGoing = RockFalls();
-                    } while (true);
+                        RockFalling(rock, ref push, ref streamIndex);
+                        //PrintChamber();
+                    }
                     //chamber.Add("|.......|"); // only add as many as needed
                     //chamber.Add("|.......|");
                     //chamber.Add("|.......|");
+                    currentTop = Math.Max(currentTop, rock.Y + rock.Height - 1);
                 }
 
-
-                return FormatResult(0, "not yet implemented");
+                return FormatResult(currentTop, "rock tower height");
             }
 
-            private bool RockFalls()
+            private void RockFalling(Rock rock, ref bool push, ref int streamIndex)
             {
-                return false;
+                int yCache = rock.Y;
+                int xCache = rock.X;
+
+                // try step
+                if (push) // streams push sideways
+                {
+                    if (InputData[0][streamIndex++ % InputData[0].Length] == '>') rock.X++;
+                    else rock.X--;
+                }
+                else // fall down
+                {
+                    rock.Y--;
+                }
+                bool collision = false;
+                for (int y = 0; y < rock.Height; y++)
+                {
+                    for (int x = 0; x < rock.Width; x++)
+                    {
+                        if (rock.Shape[y, x])
+                        {
+                            if (CheckIntersect(rock.Y + y, rock.X + x, false) > 0) collision = true;
+                        }
+                    }
+                }
+
+                if (collision)
+                {  // didn't work
+                    rock.Y = yCache;
+                    rock.X = xCache;
+                    if (!push) rock.Floating = false; // can only rest if falling
+                }
+                push = !push;
             }
 
             private void PrintChamber()
             {
-                for (int y = currentTop+8; y >= 0; y--)
+                //for (int y = currentTop + 8; y >= 0; y--)
+                for (int y = rocks.Last().Y + rocks.Last().Height - 1; y >= 0; y--)
                 {
-                    if (y == 0) Console.WriteLine("+-------+");
-                    else
+                    StringBuilder sb = new();
+                    for (int x = -1; x <= 7; x++)
                     {
-                        StringBuilder sb = new();
-                        for (int x = 0; x < 7; x++)
-                        {
-                            int isRock = 0;
-                            foreach (var rock in rocks)
-                            {
-                                int yRock = y - rock.Y;
-                                int xRock = x - rock.X;
-                                if (yRock >= 0 && yRock < rock.Height && xRock >= 0 && xRock < rock.Width)
-                                {
-                                    if (rock.Shape[yRock, xRock])
-                                    {
-                                        isRock = 1;
-                                        break;
-                                    }
-                                }
-                            }
-                            sb.Append(isRock > 0 ? '#' : '.');
-                        }
-                        Console.WriteLine($"|{sb}|");
+                        //int isRock = 0;
+                        //foreach (var rock in rocks)
+                        //{
+                        //    int yRock = y - rock.Y;
+                        //    int xRock = x - rock.X;
+                        //    if (yRock >= 0 && yRock < rock.Height && xRock >= 0 && xRock < rock.Width)
+                        //    {
+                        //        if (rock.Shape[yRock, xRock])
+                        //        {
+                        //            isRock = 1;
+                        //            break;
+                        //        }
+                        //    }
+                        //}
+                        sb.Append(CheckIntersect(y, x, true) switch { 1 => '#', 2 => '@', 3 => '|', 4 => '=', _ => '.' });
                     }
+                    Console.WriteLine($"{sb}");
                 }
+                Console.WriteLine(string.Empty);
+            }
+
+            private int CheckIntersect(int y, int x, bool includeFloating)
+            {
+                // 0 -> good
+                // 1 -> other rock
+                // 2 -> floating rock
+                // 3 -> wall
+                // 4 -> floor
+                if (x < 0 || x > 6) return 3;
+                if (y <= 0) return 4;
+
+                foreach (var rock in rocks)
+
+                    //check other rocks
+                    if (includeFloating || !rock.Floating)
+                    {
+                        {
+                            int yRock = y - rock.Y;
+                            int xRock = x - rock.X;
+                            if (yRock >= 0 && yRock < rock.Height && xRock >= 0 && xRock < rock.Width) if (rock.Shape[yRock, xRock]) return rock.Floating ? 2 : 1;
+                        }
+                    }
+                return 0;
             }
 
             private class Rock
@@ -99,7 +161,7 @@ namespace Puzzles
                 //                                          |
                 //                                        x = 0
                 public bool[,] Shape;
-                public bool floating = true;
+                public bool Floating = true;
                 public int Y;
                 public int X;
                 public int Height;
