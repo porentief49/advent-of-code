6d42b88bce7d22c0dfa95e38f588b0c4e98320d0
grr <rainer.gruber@mindscale.com>
2023-12-04 08:13:44 +0100

    day 04 part 1 done

---
+++ Puzzles/2023/Day04.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Puzzles {
+    public partial class Year2023 {
+        public class Day04 : DayBase {
+
+            protected override string Title { get; } = "Day 3: Gear Ratios";
+
+            public override void SetupAll() {
+                AddInputFile(@"2023\04_Example.txt");
+                AddInputFile(@"2023\04_rAiner.txt");
+            }
+
+            public override void Init(string InputFile) => InputData = ReadFile(InputFile, true);
+
+            public override string Solve(bool Part1) {
+                if (!Part1) return "";
+                var game = InputData.Select(i => new Card(i)).ToList();
+                int totalScore = 0;
+                for (int i = 0; i < game.Count; i++) if (game[i].MatchCount > 0) totalScore += (int)Math.Pow(2, game[i].MatchCount - 1);
+                return totalScore.ToString();
+            }
+
+            private class Card {
+                public List<int> Winning;
+                public List<int> Mine;
+                public readonly int MatchCount;
+
+                public Card(string line) {
+                    var split = line.Replace(':', '|').Split('|', StringSplitOptions.RemoveEmptyEntries);
+                    Winning = split[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToList();
+                    Mine = split[2].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToList();
+                    MatchCount = Winning.Intersect(Mine).Count();
+                }
+
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,30 +2,6 @@
 
 ![](pix/aoc.png)
 
-| # | Fritz | Lilli | rAiner |
-|---|:-:|:-:|:-:|
-| Day 1: Calorie Counting | * |  | ** |
-| Day 2: Rock Paper Scissors |  |  | ** |
-| Day 3: Rucksack Reorganization |  |  | ** |
-| Day 4: Camp Cleanup |  |  | ** |
-| Day 5: Supply Stacks |  |  | ** |
-| Day 6: Tuning Trouble |  |  | ** |
-| Day 7: No Space Left On Device |  |  | ** |
-| Day 8: Treetop Tree House |  |  | ** |
-| Day 9: Rope Bridge |  |  | ** |
-| Day 10: Cathode-Ray Tube |  |  | ** |
-| Day 11: Monkey in the Middle |  |  | ** |
-| Day 12: Hill Climbing Algorithm |  |  | ** |
-| Day 13: Distress Signal |  |  | ++ |
-| Day 14: Regolith Reservoir |  |  | ++ |
-| Day 15: Beacon Exclusion Zone |  |  | ++ |
-| Day 16: |  |  |  |
-| Day 17: |  |  |  |
-| Day 18: |  |  |  |
-| Day 19: |  |  |  |
-| Day 20: |  |  |  |
-| Day 21: |  |  |  |
-| Day 22: |  |  |  |
-| Day 23: |  |  |  |
-| Day 24: |  |  |  |
-| Day 25: |  |  |  |
+| 2021 | 2022 | 2023 |
+|---|---|---|
+| 50 ⭐ | 31 ⭐ | 6 ⭐ |
