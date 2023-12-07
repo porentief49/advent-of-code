2fb4459d0e0a0947e0de04f8e04227f64b6c5ce4
grr <rainer.gruber@mindscale.com>
2023-12-07 19:34:14 +0100

    refactor 7

--- Puzzles/2023/Day07.cs
+++ Puzzles/2023/Day07.cs
@@ -20,99 +20,71 @@ namespace Puzzles {
 
             public override string Solve(bool Part1) {
                 if (Part1) {
-                    var hands1 = InputAsLines.Select(i => new Hand1(i));
+                    var hands1 = InputAsLines.Select(i => new Hand1(i, "23456789TJQKA"));
                     var sorted = hands1.OrderBy(h => h).ToList();
                     return sorted.Select((s, i) => s.Bid * ((ulong)i + 1)).Aggregate((x, y) => x + y).ToString();
                 } else {
-                    var hands2 = InputAsLines.Select(i => new Hand2(i));
+                    var hands2 = InputAsLines.Select(i => new Hand2(i, "J23456789TQKA"));
                     var sorted = hands2.OrderBy(h => h).ToList();
                     return sorted.Select((s, i) => s.Bid * ((ulong)i + 1)).Aggregate((x, y) => x + y).ToString();
                 }
             }
 
-            private enum HandType {
-                FiveOfAKind = 6,
-                FourOfAKind = 5,
-                FullHouse = 4,
-                ThreeOfAKind = 3,
-                TwoPair = 2,
-                OnePair = 1,
-                HighCard = 0
-            }
+            private enum HandType { FiveOfAKind = 6, FourOfAKind = 5, FullHouse = 4, ThreeOfAKind = 3, TwoPair = 2, OnePair = 1, HighCard = 0 }
 
             private class Hand2 : IComparable {
                 public string Cards;
-                public List<string> CardsJoker = new();
+                public List<string> CardPermutaions = new();
                 public int[] CardStrengths;
                 public ulong Bid;
                 public HandType Type;
                 //public HandType TypeJoker;
 
-                public Hand2(string line) {
+                public Hand2(string line, string strengths) {
                     var split = line.Split(' ');
                     Cards = split[0];
                     Bid = ulong.Parse(split[1]);
-                    CardsJoker.Add(Cards);
+                    CardPermutaions.Add(Cards);
                     for (int i = 0; i < Cards.Length; i++) {
                         if (Cards[i] == 'J') {
-                            var cardCount = CardsJoker.Count; // cache as we change the colleciton
+                            var cardCount = CardPermutaions.Count; // cache as we change the colleciton
                             for (int ii = 0; ii < cardCount; ii++) {
-                                var sb = new StringBuilder(CardsJoker[ii]);
+                                var sb = new StringBuilder(CardPermutaions[ii]);
                                 for (int iii = 2; iii <= 9; iii++) {
                                     sb[i] = iii.ToString()[0];
-                                    CardsJoker.Add(sb.ToString());
+                                    CardPermutaions.Add(sb.ToString());
                                 }
                                 sb[i] = 'A';
-                                CardsJoker.Add(sb.ToString());
+                                CardPermutaions.Add(sb.ToString());
                                 sb[i] = 'K';
-                                CardsJoker.Add(sb.ToString());
+                                CardPermutaions.Add(sb.ToString());
                                 sb[i] = 'Q';
-                                CardsJoker.Add(sb.ToString());
+                                CardPermutaions.Add(sb.ToString());
                                 sb[i] = 'T';
-                                CardsJoker.Add(sb.ToString());
+                                CardPermutaions.Add(sb.ToString());
                             }
                         }
                     }
 
 
                     // card strengths
-                    CardStrengths = new int[Cards.Length];
-                    for (int i = 0; i < Cards.Length; i++) {
-                        switch (Cards[i]) {
-                            case 'A':
-                                CardStrengths[i] = 14;
-                                break;
-                            case 'K':
-                                CardStrengths[i] = 13;
-                                break;
-                            case 'Q':
-                                CardStrengths[i] = 12;
-                                break;
-                            case 'J':
-                                CardStrengths[i] = 1;
-                                break;
-                            case 'T':
-                                CardStrengths[i] = 10;
-                                break;
-                            default:
-                                CardStrengths[i] = int.Parse(Cards[i].ToString());
-                                break;
-                        }
-                    }
+                    CardStrengths = Cards.Select(c => strengths.IndexOf(c)).ToArray();
+
 
                     // hand type
                     Type = HandType.HighCard; // start with worst
-                    foreach (var cards in CardsJoker) {
-                        var distinct = cards.Distinct().ToList();
-                        var counts = distinct.Select(d => cards.Where(c => c == d).Count()).ToList();
-                        HandType thisType;
+                    foreach (var cards in CardPermutaions) {
+                        //var distinct = cards.Distinct().ToList();
+                        var counts = cards.Distinct().Select(d => cards.Where(c => c == d).Count()).ToList();
+                        HandType thisType = HandType.HighCard;
                         if (counts[0] == 5) thisType = HandType.FiveOfAKind;
                         else if (counts.Any(c => c == 4)) thisType = HandType.FourOfAKind;
                         else if (counts.Count == 2) thisType = HandType.FullHouse;
-                        else if (counts.Count == 3 && counts.Any(c => c == 3)) thisType = HandType.ThreeOfAKind;
-                        else if (counts.Count == 3) thisType = HandType.TwoPair;
+                        //else if (counts.Count == 3 && counts.Any(c => c == 3)) thisType = HandType.ThreeOfAKind;
+                        //else if (counts.Count == 3) thisType = HandType.TwoPair;
+                        else if (counts.Count == 3) thisType = counts.Any(c => c == 3) ? HandType.ThreeOfAKind : HandType.TwoPair;
                         else if (counts.Count == 4) thisType = HandType.OnePair;
-                        else thisType = HandType.HighCard;
+                        //else thisType = HandType.HighCard;
                         if (thisType > Type) Type = thisType;
                     }
                 }
@@ -135,48 +107,20 @@ namespace Puzzles {
                 public ulong Bid;
                 public HandType Type;
 
-                public Hand1(string line) {
+                public Hand1(string line, string strengths) {
                     var split = line.Split(' ');
                     Cards = split[0];
                     Bid = ulong.Parse(split[1]);
-
-                    // card strengths
-                    CardStrengths = new int[Cards.Length];
-                    for (int i = 0; i < Cards.Length; i++) {
-                        switch (Cards[i]) {
-                            case 'A':
-                                CardStrengths[i] = 14;
-                                break;
-                            case 'K':
-                                CardStrengths[i] = 13;
-                                break;
-                            case 'Q':
-                                CardStrengths[i] = 12;
-                                break;
-                            case 'J':
-                                CardStrengths[i] = 11;
-                                break;
-                            case 'T':
-                                CardStrengths[i] = 10;
-                                break;
-                            default:
-                                CardStrengths[i] = int.Parse(Cards[i].ToString());
-                                break;
-                        }
-                    }
+                    CardStrengths = Cards.Select(c => strengths.IndexOf(c)).ToArray();
 
                     // hand type
-                    var distinct = Cards.Distinct().ToList();
-                    var counts = distinct.Select(d => Cards.Where(c => c == d).Count()).ToList();
+                    var counts = Cards.Distinct().Select(d => Cards.Where(c => c == d).Count()).ToList();
                     if (counts[0] == 5) Type = HandType.FiveOfAKind;
                     else if (counts.Any(c => c == 4)) Type = HandType.FourOfAKind;
                     else if (counts.Count == 2) Type = HandType.FullHouse;
-                    else if (counts.Count == 3 && counts.Any(c => c == 3)) Type = HandType.ThreeOfAKind;
-                    else if (counts.Count == 3) Type = HandType.TwoPair;
+                    else if (counts.Count == 3) Type = counts.Any(c => c == 3) ? HandType.ThreeOfAKind : HandType.TwoPair;
                     else if (counts.Count == 4) Type = HandType.OnePair;
                     else Type = HandType.HighCard;
-
-
                 }
 
                 public int CompareTo(object? incomingHand) {
