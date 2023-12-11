fa687912983119518a2fbc42a7b1c5b9052102ed
grr <rainer.gruber@mindscale.com>
2023-12-11 12:02:47 +0100

    further cleanup

--- Puzzles/2023/Day11.cs
+++ Puzzles/2023/Day11.cs
@@ -1,4 +1,6 @@
-﻿namespace Puzzles {
+﻿using System.Net.Sockets;
+
+namespace Puzzles {
 
     public partial class Year2023 {
 
@@ -15,12 +17,12 @@
 
             public override string Solve(bool part1) {
                 List<string> rows = InputAsLines.ToList();
-                Galaxy.EmptyReplaceWith = part1 ? 2 : 1000000;
                 Galaxy.EmptyRows = Enumerable.Range(0, rows.Count).Where(r => rows[r].All(ch => ch == '.')).ToList();
                 Galaxy.EmptyCols = Enumerable.Range(0, rows[0].Length).Where(c => rows.Select(r => r[c]).All(c => c == '.')).ToList();
                 var galaxies = new List<Galaxy>();
                 for (int row = 0; row < rows.Count; row++) for (int col = 0; col < rows[row].Length; col++) if (rows[row][col] == '#') galaxies.Add(new Galaxy(row, col));
                 long totalDist = 0;
+                Galaxy.EmptyReplaceWith = part1 ? 2 : 1000000;
                 for (int i = 0; i < galaxies.Count; i++) for (int ii = i + 1; ii < galaxies.Count; ii++) totalDist += galaxies[i].ManhattanDist(galaxies[ii]);
                 return totalDist.ToString();
             }
@@ -37,14 +39,11 @@
                     Col = col;
                 }
 
-                public long ManhattanDist(Galaxy other) {
-                    long fromRow = Math.Min(Row, other.Row);
-                    long toRow = Math.Max(Row, other.Row);
-                    long rows = (toRow - fromRow) + EmptyRows.Where(r => r >= fromRow && r <= toRow).Count() * (EmptyReplaceWith - 1);
-                    long fromCol = Math.Min(Col, other.Col);
-                    long toCol = Math.Max(Col, other.Col);
-                    long cols = (toCol - fromCol) + EmptyCols.Where(c => c >= fromCol && c <= toCol).Count() * (EmptyReplaceWith - 1);
-                    return rows + cols;
+                public long ManhattanDist(Galaxy other) => DistWithEmpty(Row, other.Row, EmptyRows) + DistWithEmpty(Col, other.Col, EmptyCols);
+
+                private long DistWithEmpty(long from, long to, List<int> empty) {
+                    (long min, long max) = from < to ? (from, to) : (to, from);
+                    return max - min + empty.Where(e => e >= min && e <= max).Count() * (EmptyReplaceWith - 1);
                 }
             }
         }
