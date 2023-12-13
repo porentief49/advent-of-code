b9ef654b73d8e25e8ca3f70bdba9975f80c7dadf
grr <rainer.gruber@mindscale.com>
2023-12-13 08:48:56 +0100

    day 13 clean

--- AutoTests/_Year2023.cs
+++ AutoTests/_Year2023.cs
@@ -316,16 +316,16 @@ namespace _Year2023 {
         }
 
         [TestMethod]
-        public void _Part1_Example() => Helpers.RunTest("Example", true, "");
+        public void _Part1_Example() => Helpers.RunTest("Example", true, "405");
 
         [TestMethod]
-        public void _Part2_Example() => Helpers.RunTest("Example", false, "");
+        public void _Part2_Example() => Helpers.RunTest("Example", false, "400");
 
         [TestMethod]
-        public void _Part1_rAiner() => Helpers.RunTest("rAiner", true, "");
+        public void _Part1_rAiner() => Helpers.RunTest("rAiner", true, "30802");
 
         [TestMethod]
-        public void _Part2_rAiner() => Helpers.RunTest("rAiner", false, "");
+        public void _Part2_rAiner() => Helpers.RunTest("rAiner", false, "37876");
     }
 
     [TestClass]
--- Puzzles/2023/Day13.cs
+++ Puzzles/2023/Day13.cs
@@ -14,128 +14,43 @@
             public override void Init(string inputFile) => InputAsText = ReadText(inputFile, true);
 
             public override string Solve() {
-                if (Part1) {
-                    var patterns = InputAsText.Split("\n\n", StringSplitOptions.RemoveEmptyEntries).Select(i => new Pattern(i)).ToList();
-                    return patterns.Select(p => p.result).Sum().ToString();
-                }
-                var pat2 = InputAsText.Split("\n\n", StringSplitOptions.RemoveEmptyEntries).Select(i => new Pattern2(i)).ToList();
-                return pat2.Select(p => p.result).Sum().ToString();
+                var pat2 = InputAsText.Split("\n\n", StringSplitOptions.RemoveEmptyEntries).Select(i => new Pattern(i, Part1 ? 0 : 1)).ToList();
+                return pat2.Select(p => p.Result).Sum().ToString();
             }
 
-            private class Pattern2 {
+            private class Pattern {
                 public char[][] map; // row, column
-                public bool isVertical;
-                public int leftOrAbove;
-                public int result;
+                public int Result = 0;
 
-                public Pattern2(string input) {
+                public Pattern(string input, int smudges) {
                     var split = input.Split("\n", StringSplitOptions.RemoveEmptyEntries);
                     map = split.Select(s => s.ToCharArray()).ToArray();
-
-                    //try vertical
-                    //bool foundIt;
                     int smudgeCount = 0;
                     int pos = 0;
-                    do {
-                        //foundIt = true; // assume best
+                    do { // look for vertical
                         smudgeCount = 0;
                         for (int col = 0; col <= pos; col++) {
-                            // pos = 0, c = 0, otherCol = 1;
-                            // pos = 3, c = 3, otherCol = 4;
-                            // pos = 3, c = 2, otherCol = 5;
-                            // pos = 3, c = 0, otherCol = 7;
                             int otherCol = pos + (pos - col) + 1;
-                            if (otherCol < map[0].Length) {
-                                for (int row = 0; row < map.Length; row++) {
-                                    //if (map[row][col] != map[row][otherCol]) foundIt = false;
-                                    if (map[row][col] != map[row][otherCol]) smudgeCount++;
-                                }
-                            }
+                            if (otherCol < map[0].Length) for (int row = 0; row < map.Length; row++) if (map[row][col] != map[row][otherCol]) smudgeCount++;
                         }
                         pos++;
-                    //} while (!foundIt && pos < map[0].Length - 1);
-                    } while (smudgeCount!=1 && pos < map[0].Length - 1);
-                    isVertical = true;
-                    leftOrAbove = pos;
-
-                    //try horizontal
-                    //if (!foundIt) {
-                    if (smudgeCount != 1) {
+                    } while (smudgeCount != smudges && pos < map[0].Length - 1);
+                    if (smudgeCount == smudges) {
+                        Result = pos;
+                        //return;
+                    } else {
                         pos = 0;
-                        do {
-                            //foundIt = true; // assume best
+                        do { // look for horizontal
                             smudgeCount = 0;
                             for (int row = 0; row <= pos; row++) {
                                 int otherRow = pos + (pos - row) + 1;
-                                if (otherRow < map.Length) {
-                                    for (int col = 0; col < map[0].Length; col++) {
-                                        //if (map[row][col] != map[otherRow][col]) foundIt = false;
-                                        if (map[row][col] != map[otherRow][col]) smudgeCount++;
-                                    }
-                                }
+                                if (otherRow < map.Length) for (int col = 0; col < map[0].Length; col++) if (map[row][col] != map[otherRow][col]) smudgeCount++;
                             }
                             pos++;
-                        //} while (!foundIt && pos < map.Length - 1);
-                        } while (smudgeCount != 1 && pos < map.Length - 1);
-                        isVertical = false;
-                        leftOrAbove = pos;
+                        } while (smudgeCount != smudges && pos < map.Length - 1);
+                        Result = pos * 100;
                     }
-                    result = leftOrAbove * (isVertical ? 1 : 100);
-                    Console.WriteLine($"vertical: {isVertical}, left/above: {leftOrAbove}");
-                }
-            }
-            private class Pattern {
-                public char[][] map; // row, column
-                public bool isVertical;
-                public int leftOrAbove;
-                public int result;
-
-                public Pattern(string input) {
-                    var split = input.Split("\n", StringSplitOptions.RemoveEmptyEntries);
-                    map = split.Select(s => s.ToCharArray()).ToArray();
-
-                    //try vertical
-                    bool foundIt;
-                    int pos = 0;
-                    do {
-                        foundIt = true; // assume best
-                        for (int col = 0; col <= pos; col++) {
-                            // pos = 0, c = 0, otherCol = 1;
-                            // pos = 3, c = 3, otherCol = 4;
-                            // pos = 3, c = 2, otherCol = 5;
-                            // pos = 3, c = 0, otherCol = 7;
-                            int otherCol = pos + (pos - col) + 1;
-                            if (otherCol < map[0].Length) {
-                                for (int row = 0; row < map.Length; row++) {
-                                    if (map[row][col] != map[row][otherCol]) foundIt = false;
-                                }
-                            }
-                        }
-                        pos++;
-                    } while (!foundIt && pos < map[0].Length - 1);
-                    isVertical = true;
-                    leftOrAbove = pos;
-
-                    //try horizontal
-                    if (!foundIt) {
-                        pos = 0;
-                        do {
-                            foundIt = true; // assume best
-                            for (int row = 0; row <= pos; row++) {
-                                int otherRow = pos + (pos - row) + 1;
-                                if (otherRow < map.Length) {
-                                    for (int col = 0; col < map[0].Length; col++) {
-                                        if (map[row][col] != map[otherRow][col]) foundIt = false;
-                                    }
-                                }
-                            }
-                            pos++;
-                        } while (!foundIt && pos < map.Length - 1);
-                        isVertical = false;
-                        leftOrAbove = pos;
-                    }
-                    result = leftOrAbove * (isVertical ? 1 : 100);
-                    Console.WriteLine($"vertical: {isVertical}, left/above: {leftOrAbove}");
+                    if (smudgeCount != smudges) throw new Exception("could not find  mirror");
                 }
             }
         }
