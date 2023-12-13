04f543976ffff1297e9df93015e05f97b0f0a675
grr <rainer.gruber@mindscale.com>
2023-12-13 18:42:30 +0100

    refactoring

--- Puzzles/2023/Day13.cs
+++ Puzzles/2023/Day13.cs
@@ -15,40 +15,37 @@
 
             public override string Solve() {
                 var pattern = InputAsText.Split("\n\n", StringSplitOptions.RemoveEmptyEntries).Select(i => new Pattern(i, Part1 ? 0 : 1)).ToList();
-                return pattern.Select(p => p.Result).Sum().ToString();
+                return pattern.Select(p => p.Position).Sum().ToString();
             }
 
             private class Pattern {
-                public char[][] map; // row, column
-                public int Result = 0;
+                public int Position = 0;
 
                 public Pattern(string input, int smudges) {
+                    char[][] map; // row, column
                     var split = input.Split("\n", StringSplitOptions.RemoveEmptyEntries);
                     map = split.Select(s => s.ToCharArray()).ToArray();
                     int smudgeCount = 0;
-                    int pos = 0;
+                    Position = 0;
                     do { // look for vertical
                         smudgeCount = 0;
-                        for (int col = 0; col <= pos; col++) {
-                            int otherCol = pos + (pos - col) + 1;
+                        for (int col = 0; col <= Position; col++) {
+                            int otherCol = Position * 2 - col + 1;
                             if (otherCol < map[0].Length) for (int row = 0; row < map.Length; row++) if (map[row][col] != map[row][otherCol]) smudgeCount++;
                         }
-                        pos++;
-                    } while (smudgeCount != smudges && pos < map[0].Length - 1);
-                    if (smudgeCount == smudges) Result = pos;
-                    else {
-                        pos = 0;
+                    } while (++Position < map[0].Length - 1 && smudgeCount != smudges);
+                    if (smudgeCount != smudges) {
+                        Position = 0;
                         do { // look for horizontal
                             smudgeCount = 0;
-                            for (int row = 0; row <= pos; row++) {
-                                int otherRow = pos + (pos - row) + 1;
+                            for (int row = 0; row <= Position; row++) {
+                                int otherRow = Position * 2 - row + 1;
                                 if (otherRow < map.Length) for (int col = 0; col < map[0].Length; col++) if (map[row][col] != map[otherRow][col]) smudgeCount++;
                             }
-                            pos++;
-                        } while (smudgeCount != smudges && pos < map.Length - 1);
-                        Result = pos * 100;
+                        } while (++Position < map.Length - 1 && smudgeCount != smudges);
+                        Position *= 100; // horizontal * 100 for total sum
                     }
-                    if (smudgeCount != smudges) throw new Exception("could not find  mirror");
+                    if (smudgeCount != smudges) throw new Exception("could not find mirror");
                 }
             }
         }
