f25933d2ef84f28506bdd17bf29a84f6d6aef76e
grr <rainer.gruber@mindscale.com>
2023-12-14 08:02:21 +0100

    14/1 working

--- Puzzles/2023/Day14.cs
+++ Puzzles/2023/Day14.cs
@@ -4,17 +4,42 @@
 
         public class Day14 : DayBase {
 
-            protected override string Title { get; } = "Day 14: ???";
+            protected override string Title { get; } = "Day 14: Parabolic Reflector Dish";
 
             public override void SetupAll() {
                 AddInputFile(@"2023\14_Example.txt");
-                //AddInputFile(@"2023\14_rAiner.txt");
+                AddInputFile(@"2023\14_rAiner.txt");
             }
 
             public override void Init(string inputFile) => InputAsLines = ReadLines(inputFile, true);
 
+            //private enum Direction { N, E, S, W}
+
             public override string Solve() {
-                return "";
+                if (Part2) return "";
+                var platform = InputAsLines.Select(i => i.ToCharArray()).ToArray();
+                //PrintGrid(platform);
+                // tilt north
+                bool stillRolling;
+                do {
+                    stillRolling = false;
+                    for (int r = 1; r < platform.Length; r++) {
+                        for (int c = 0; c < platform[r].Length; c++) {
+                            if (platform[r][c] == 'O' && platform[r - 1][c] == '.') {
+                                (platform[r][c], platform[r - 1][c]) = (platform[r - 1][c], platform[r][c]);
+                                stillRolling = true;
+                            }
+                        }
+                    }
+                    //PrintGrid(platform);
+                } while (stillRolling);
+
+                // count results
+                int load = 0;
+                for (int r = 0; r < platform.Length; r++) {
+                    load += platform[r].Count(c => c == 'O') * (platform.Length - r);
+                }
+                return load.ToString();
             }
         }
     }
